353a64178d6fd20211f033b807b093e3853b75e5
Daniel Esposito <danielesposito@macbkk.com>
2016-03-03 20:20:33 +0700

    add a new variable

--- MyGitFun/ViewController.swift
+++ MyGitFun/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     //making a change
+    var thisText: String = "My Testcx"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
